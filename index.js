e1248b2d98a53eafaa108247b4dac600e011c364
ismahaneyy <liciousmayrah@gmail.com>
2022-11-29 14:48:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    const Win = record.find((record) => record.result === "W");
+    if (Win) {
+        return Win.year;
+    } else {
+        return undefined;
+    }
+}
+    
+    
